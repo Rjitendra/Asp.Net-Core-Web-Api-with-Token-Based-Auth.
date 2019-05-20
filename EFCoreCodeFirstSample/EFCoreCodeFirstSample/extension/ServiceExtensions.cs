c0aac7e3608d5466799fbca9f8efb106407eb5e5
jitendra <jbehera@bridgetree.com>
2019-05-20 14:40:08 +0530

    set up project

---
+++ EFCoreCodeFirstSample/EFCoreCodeFirstSample/extension/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using LoggerService;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstSample.extension
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
