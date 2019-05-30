78ab5b9b302ae349ec5e92861f3b8ea646919259
jitendra <jbehera@bridgetree.com>
2019-05-30 11:59:36 +0530

    cccc

--- EFCoreCodeFirstSample/Service/RepositoryBase.cs
+++ EFCoreCodeFirstSample/Service/RepositoryBase.cs
@@ -2,10 +2,8 @@
 using Model.Contexts;
 using Service.Interface;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace Service
 {
