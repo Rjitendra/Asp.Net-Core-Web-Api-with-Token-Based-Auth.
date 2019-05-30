63995741c852d6e068913ca0604fcda787027f20
jitendra <jbehera@bridgetree.com>
2019-05-30 16:53:06 +0530

    asss

--- EFCoreCodeFirstSample/Service/Interface/IOwnerRepository.cs
+++ EFCoreCodeFirstSample/Service/Interface/IOwnerRepository.cs
@@ -8,5 +8,8 @@ namespace Service.Interface
     public interface IOwnerRepository : IRepositoryBase<Owner>
     {
         IEnumerable<Owner> GetAllOwners();
+        Owner GetOwnerById(Guid ownerId);
+        OwnerExtended GetOwnerWithDetails(Guid ownerId);
+        void CreateOwner(Owner owner);
     }
 }
